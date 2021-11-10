1b83d8a919ae8d791ac6e8da246520bcaa96c906
Pier <pierfrancesco297@gmail.com>
2021-11-10 15:50:38 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var app = new Vue(
         el: '#myapp',
         data: {
             titolo: 'Hello World',
+            myImage: 'https://www.lenny76.com/wp-content/uploads/2018/03/hello-world.jpg'
         },
-        
     }
 );
